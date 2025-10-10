0f9ef10d54ea6f1604e010e7533a5f4e4c99810f
Kelvin <edeh47245@gmail.com>
2025-10-10 20:14:57 +0100

    Login php file

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in</title>
+</head>
+<body>
+    <form >
+        <input type="text" name = "username">
+
+        <input type="password">
+        <button>Log in</button>
+        <a href="signup.php" >Click to sign up</a>
+        
+    </form>
+</body>
+</html>
